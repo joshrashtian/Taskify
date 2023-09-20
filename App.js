08a29d37c6f06979ba37be82bd39b47a4fb3568f
Joshua Rashtian <joshrashtian1@gmail.com>
2023-09-20 15:15:08 -0700

    Starting

---
+++ App.js
@@ -0,0 +1,111 @@
+
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import Task from './components/Task';
+import React, {useState} from 'react';
+import { useFonts } from 'expo-font';
+
+export default function App() {
+{/* Set Up Some Simples */}
+  const [fontsLoaded] = useFonts({
+    'Cheese-Burger': require('./assets/fonts/CheeseBurger.otf')
+  });
+
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+  const handleAddTask = () => {
+    setTaskItems([...taskItems, task])
+    setTask(null);
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
+
+  return (
+    <View style={styles.container}>
+      {/* Today's Tasks */}
+      <View style={styles.headingtext}>
+        <Text style={styles.sectionTitle}>Taskify</Text>
+        <View style={styles.items}>
+          {/* Where Tasks Will Go */}
+          {
+            taskItems.map((item, index) => {
+              return (
+                <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                <Task text={item} />
+                </TouchableOpacity>
+              )
+            })
+          }
+        </View>
+      </View>
+
+
+  <KeyboardAvoidingView behavior ={Platform.OS === "ios" ? "padding" : "height"} style = {styles.writeTaskWrapper}>
+      <TextInput style={styles.input} placeholder={'Type tasks...'} value={task} onChangeText={text => setTask(text)} />
+
+    <TouchableOpacity onPress={() => handleAddTask()}>
+      <View style ={styles.addButton}>
+        <Text style={styles.addText}>+</Text>
+      </View>
+    </TouchableOpacity>
+  </KeyboardAvoidingView>
+  </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#D3D3D3',
+  },
+  headingtext: {
+    paddingTop: 80,
+    paddingLeft: 20,
+    paddingRight: 20,
+    flex: 3,
+    backgroundColor: '#EFEFEF',
+  },
+  sectionTitle: {
+    color: '#55BCF6',
+    fontSize: 50,
+    fontWeight: 'bold',
+    fontFamily: 'Cheese-Burger',
+  },
+  items: {
+    marginTop: 30,
+  },
+writeTaskWrapper: {
+  position: 'absolute', 
+  bottom: '10%',
+  width: '100%',
+  flexDirection: 'row',
+  justifyContent: 'space-around',
+},
+input: {
+  paddingVertical: 15,
+  paddingHorizontal: 15,
+  backgroundColor: '#FFF',
+  borderRadius: 60,
+  borderColor: '#E3E3E3',
+  borderWidth: 0.5,
+  width: '75%',
+  marginLeft: 5,
+},
+addButton: {
+  width: 60,
+  height: 60,
+  backgroundColor: '#B3DBE6',
+  borderRadius: 75,
+  justifyContent: 'center',
+  alignItems: 'center',
+  marginRight: 5,
+},
+addText: {
+  fontSize: 30,
+},
+
+});
